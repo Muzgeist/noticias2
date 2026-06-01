5c3392c86a55e75140ac1fe49092215fdb76c149
Felipe Shedler Ferreira Santos Zulmiro <1051966@senacminas.edu.br>
2026-06-01 16:25:05 -0300

    release 1.2

--- backend/js/server.js
+++ backend/js/server.js
@@ -1,56 +1,104 @@
 import express from 'express';
 import executarQuery from './db.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/noticias', async (req, res) => {
     let query = `
         SELECT
+            id,
             titulo,
             conteudo,
             caminhoImagem,
             link
         FROM
             noticias
+        ORDER BY
+            id DESC
+        LIMIT 10
         `;
 
         let resultado = await executarQuery(query);
         res.send(resultado[0]);
 });
 
-app.get('/check', async (req, res) => {
-    var status = {
-        status: "Running.."
-    };
-    res.send(status);
-});
+app.post('/noticias', async (req, res) => {
 
-app.post('/check', async (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+    var Query = `
+        INSERT INTO noticias (
+            titulo,
+            conteudo,
+            caminhoImagem,
+            link
+        ) VALUES (
+            ?,
+            ?,
+            ?,
+            ? 
+        )
+    `;
 
-app.get('/hello', async (req, res) => {
-    var hello = {
-        hello: "Hello from Teacher!"
-    };
-    res.send(hello);
-});
+    var noticia = [
+        req.body.titulo,
+        req.body.conteudo,
+        req.body.caminhoImagem,
+        req.body.link
+    ];
 
-app.post('/hello', async (req, res) => {
-    try {
+
+
+    let resultado = await executarQuery(query, noticia);
+
+    try{
         res.send({
-            hello: `Felipe diz olá pra você ${req.body.name}`
-        })
+            insertId: resultado[0].insertId
+        });
     }
-    catch{
+    catch {
         res.send({
-            hello: 'fail'
-        })
+            insertId: null
+        });
     }
+    
 });
+
+// app.get('/check', async (req, res) => {
+//     var status = {
+//         status: "Running.."
+//     };
+//     res.send(status);
+// });
+
+// app.post('/check', async (req, res) => {
+//     console.log(req.body);
+//     res.send(req.body);
+// });
+
+// app.get('/hello', async (req, res) => {
+//     var hello = {
+//         hello: "Hello from Teacher!"
+//     };
+//     res.send(hello);
+// });
+
+// app.post('/hello', async (req, res) => {
+//     try {
+//         res.send({
+//             hello: `Felipe diz olá pra você ${req.body.name}`
+//         })
+//     }
+//     catch{
+//         res.send({
+//             hello: 'fail'
+//         })
+//     }
+// });
 
 app.listen(3000, () =>{
     console.log("Servidor online em http://localhost:3000");
 });
+
