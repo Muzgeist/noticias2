81d6919eb0cff99273ab962af0bbb1b6d5632c31
Felipe Shedler Ferreira Santos Zulmiro <1051966@senacminas.edu.br>
2026-06-22 16:14:14 -0300

    deploy

--- backend/js/db.js
+++ backend/js/db.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise.js'
 
 async function conectar(){
     const conexao = await mysql.createConnection({
-        host: '127.0.0.1',
-        port: 3306,
-        user: 'root',
-        password: '',
-        database: 'noticiario',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     });
 
     return conexao;
