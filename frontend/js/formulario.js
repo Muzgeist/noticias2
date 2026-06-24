158d3d3920fcfd2208008f5407ce646e97a52c24
Felipe Shedler Ferreira Santos Zulmiro <1051966@senacminas.edu.br>
2026-06-24 13:45:30 -0300

    deploy fixed

--- frontend/js/formulario.js
+++ frontend/js/formulario.js
@@ -49,7 +49,7 @@ async function cadastrar_noticia() {
     };
 
     try {
-        const resposta = await fetch("noticias-backend-felipe-3000.up.railway.app", {
+        const resposta = await fetch("https://noticias-frontend-felipe-production.up.railway.app/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
